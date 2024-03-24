d93938c15a01dafe2184362c13cecc5720dfd5fa
Jorel I. Agustin <42881197+katedato@users.noreply.github.com>
2024-03-24 23:31:05 +0800

    Screens update

--- RecipeOrganizerApp/App.js
+++ RecipeOrganizerApp/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from './screens/SplashScreen';
+import HomeScreen from './screens/HomeScreen';
+import CategoryScreen from './screens/CategoryScreen';
+import AddRecipeScreen from './screens/AddRecipeScreen';
+import EditRecipeScreen from './screens/EditRecipeScreen';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" headerMode="none">
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Category" component={CategoryScreen} />
+        <Stack.Screen name="AddRecipe" component={AddRecipeScreen} />
+        <Stack.Screen name="EditRecipe" component={EditRecipeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ RecipeOrganizerApp/screens/SplashScreen.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Home');
+    }, 20000); // 5 seconds delay
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Splash Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default SplashScreen;
